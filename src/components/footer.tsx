02fc869c42dd1686ec3df91d6c6e19a05dbde7ef
Yiorgos Makridakis <makridakisyiorgos@gmail.com>
2023-03-29 20:21:46 +0300

    Added socials on footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,8 @@
-import Image from 'next/image'
+import Image from 'next/image';
 import Link from 'next/link';
+
+import { FaInstagram, FaTiktok, FaFacebookF } from 'react-icons/fa';
+
 import ActiveLink from '@/components/common/activeLink';
 
 const Footer = ({
@@ -10,11 +13,33 @@ const Footer = ({
     return (
         <div className="sticky top-0 z-10 bg-zinc-300 py-10 ">
             <div className="mx-auto">
-                <div className=" relative w-full h-[150px]">
-                    <Image className="object-contain" src="/media/logo_with_writing.png" alt="logo with writing" fill />
+                <div className=" relative h-[150px] w-full">
+                    <Image
+                        className="object-contain"
+                        src="/media/logo_with_writing.png"
+                        alt="logo with writing"
+                        fill
+                    />
                 </div>
             </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-10 pt-10">
+            <div className="flex w-full justify-center gap-10 text-white">
+                <div className="rounded-full bg-black p-2 hover:scale-110">
+                    <Link href="https://www.tiktok.com/@all_hypes">
+                        <FaInstagram className="h-6 w-6" />
+                    </Link>
+                </div>
+                <div className="rounded-full bg-black p-2 hover:scale-110">
+                    <Link href="https://www.instagram.com/allhypes_gr/">
+                        <FaTiktok className="h-6 w-6" />
+                    </Link>
+                </div>
+                <div className="rounded-full bg-black p-2 hover:scale-110">
+                    <Link href="https://www.facebook.com/people/AllHypes/100089393306777/">
+                        <FaFacebookF className="h-6 w-6" />
+                    </Link>
+                </div>
+            </div>
+            <div className="grid grid-cols-1 gap-10 pt-10 md:grid-cols-2">
                 <div className="float-none text-center md:float-right md:text-right">
                     {navLinks.map(({ title, path }, i) => (
                         <div className="py-2" key={i}>
